b6caff3ee321e4324e9b458b51a3f04f2917cbe1
boubechtoula <dw_boubechtoula@esi.dz>
2020-02-07 15:13:10 +0100

    add list-tweets component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ListTweetsComponent } from './list-tweets/list-tweets.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ListTweetsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/list-tweets/list-tweets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-tweets',
+  templateUrl: './list-tweets.component.html',
+  styleUrls: ['./list-tweets.component.css']
+})
+export class ListTweetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
