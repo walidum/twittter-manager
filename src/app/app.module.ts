bf992df0ddfea1ec3b278e1d1c1f8b60101e8e7d
boubechtoula <dw_boubechtoula@esi.dz>
2020-02-07 18:17:09 +0100

    ajouter un composant de recherche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ListTweetsComponent } from './list-tweets/list-tweets.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ListTweetsComponent ],
+  declarations: [ AppComponent, HelloComponent, ListTweetsComponent, SearchBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onFocus(){ 
+      $(".search-box").addClass("border-searching");
+      $(".search-icon").addClass("si-rotate");
+    
+
+  }
+  
+}
+
+//https://codepen.io/dharan1999/pen/zYxegze
