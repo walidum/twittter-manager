3008cf45bc322c7535a03d5bcd438b452c2092a4
boubechtoula <dw_boubechtoula@esi.dz>
2020-02-14 16:23:02 +0100

    1 file modified

--- src/app/models/tweet.ts
+++ src/app/models/tweet.ts
@@ -1,6 +1,11 @@
 export class Tweet {
   id :string; 
   text : string ; 
+  idStr : string;
+  createdAt : string;
+  fromUser : string;
+  languageCode : string;
+  source : string;
   public Tweet(id : string , text : string){
     this.id = id;
     this.text = text;
